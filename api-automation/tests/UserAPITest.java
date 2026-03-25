e84f2cc3ce2b8b4a1be2d0ee72000428a9a0c500
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-03-25 15:05:42 +0530

    Update UserAPITest.java

--- api-automation/tests/UserAPITest.java
+++ api-automation/tests/UserAPITest.java
@@ -1,1 +1,29 @@
+package tests;
 
+import base.BaseAPI;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class UserAPITest extends BaseAPI {
+
+    @Test
+    public void getUserTest() {
+        Response response = given().get("/users/2");
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    public void createUserTest() {
+        String body = "{ \"name\": \"QA\", \"job\": \"Engineer\" }";
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(body)
+                .post("/users");
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+    }
+}

fb8cf1cb2b2854f637febc2c3a6cd3c2013f9f40
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-03-25 15:05:10 +0530

    Update BaseAPI.java

--- api-automation/base/BaseAPI.java
+++ api-automation/base/BaseAPI.java
@@ -1,1 +1,12 @@
+package base;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+public class BaseAPI {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
+}

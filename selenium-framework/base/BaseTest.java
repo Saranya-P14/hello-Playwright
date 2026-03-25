0b001b7bca4c056cdc4d425e63adf4f11ecd467e
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-03-25 14:44:40 +0530

    Update BaseTest.java

--- selenium-framework/base/BaseTest.java
+++ selenium-framework/base/BaseTest.java
@@ -1,1 +1,21 @@
+package base;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.initDriver("chrome");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}

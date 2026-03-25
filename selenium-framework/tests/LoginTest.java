1bf36e81149082ea213134d758c32f93186a2e09
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-03-25 14:53:29 +0530

    Update LoginTest.java

--- selenium-framework/tests/LoginTest.java
+++ selenium-framework/tests/LoginTest.java
@@ -1,1 +1,19 @@
+package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void validLoginTest() {
+        driver.get("https://example.com");
+
+        LoginPage login = new LoginPage(driver);
+        login.login("test", "password");
+
+        Assert.assertTrue(driver.getTitle().contains("Dashboard"));
+    }
+}

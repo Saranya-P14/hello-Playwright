54f34d48d2a39271330781db619a5a977ba60223
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-03-25 14:54:05 +0530

    Update DriverFactory.java

--- selenium-framework/utils/DriverFactory.java
+++ selenium-framework/utils/DriverFactory.java
@@ -1,1 +1,15 @@
 
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver initDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            return new ChromeDriver();
+        }
+        throw new RuntimeException("Browser not supported");
+    }
+}
